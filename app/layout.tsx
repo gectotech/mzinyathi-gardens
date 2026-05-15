7ba4b0f0a7cb8b4e5f35778605b1beec276623ea
Misleydis <misleydischiremba1@gmail.com>
2026-05-15 09:38:36 +0200

    applayout and tsx update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/layout/Navbar';
-import Footer from '@/components/layout/Footer';
+import Footer from '@/components/layout/FooterTemp';
 
 const inter = Inter({ subsets: ['latin'] });
 
