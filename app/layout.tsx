5a17336ca240774f665be80596138474e235009c
Misleydis <misleydischiremba1@gmail.com>
2026-05-15 09:43:30 +0200

    update @not allowed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/layout/Navbar';
-import Footer from '@/components/layout/FooterTemp';
+import Navbar from '../components/layout/Navbar';
+import Footer from '../components/layout/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
